fec420bb52d6bf88f4600bb3d660eb694d2a33e5
KourinAz <96441011+KourinAz@users.noreply.github.com>
2024-08-24 20:58:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,8 +12,6 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-vless://85add957-48c2-4db4-d54c-abaef3be0b8b@hkbn.kourin.top:55880?encryption=none&security=none&type=ws&path=%2Fgg#HKBN
-ss://YWVzLTEyOC1nY206dWVFM0w2b3BkUA==@183.236.51.57:26986#TW%20SS
 ss://YWVzLTEyOC1nY206TlJzUjg4N1gxZ2U1ZnpCZU5XTm1IVkR1cXhrSkdZWjMwQzlkWkhPUVI0RXM=@183.236.51.57:25178#%E6%B8%AF%E5%93%A5%20SS
 vless://85add957-48c2-4db4-d54c-abaef3be0b8b@hkbn.kourin.top:55880?encryption=none&security=none&type=ws&path=%2Fgg#HKBN
 vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIlx1NzUzMlx1OUFBOFx1NjU4N1x1NEUxQ1x1NEVBQyIsDQogICJhZGQiOiAiMTYyLjE1OS4xMzcuMjM3IiwNCiAgInBvcnQiOiAiNDQzIiwNCiAgImlkIjogIjA2NjQ2M2ZmLWQ1MjgtNDJlYy05MzBhLTUwZGZhY2QzZTA2ZCIsDQogICJhaWQiOiAiMCIsDQogICJzY3kiOiAiYXV0byIsDQogICJuZXQiOiAid3MiLA0KICAidHlwZSI6ICJub25lIiwNCiAgImhvc3QiOiAidG9reW8ua291cmluLnRvcCIsDQogICJwYXRoIjogIi9kaiIsDQogICJ0bHMiOiAidGxzIiwNCiAgInNuaSI6ICIiLA0KICAiYWxwbiI6ICIiLA0KICAiZnAiOiAiIg0KfQ==
